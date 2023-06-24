22fe652934c560b561ef3b6e785bb0c8bc403776
Vinícius <vineraccolto@gmail.com>
2023-06-24 04:39:16 -0300

    Ajustes do layout do Bloco de Notas

--- src/Routes/BlocoDeNotas.jsx
+++ src/Routes/BlocoDeNotas.jsx
@@ -1,106 +1,147 @@
-import { Button, Container, Grid, Paper, TextareaAutosize } from "@mui/material";
+import { Button, Container, Paper, TextareaAutosize } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import CloseIcon from "@mui/icons-material/Close";
-import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
 import "../css/BlocoDeNotas.css";
-import * as React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
+
 function BlocoDeNotas() {
-  
-  const [open, setOpen] = React.useState(false);
 
-  const handleClick = () => {
-    setOpen(true);
+  const [titulo, setTitulo] = useState('');
+
+  const [conteudo, setConteudo] = useState('');
+
+  const [anotacoes, setAnotacoes] = useState([]);
+
+  const [alerta, setAlerta] = useState(false);
+
+  const handleChangeTitulo = (event) => {
+    setTitulo(event.target.value);
   };
 
-  const handleClose = () => {
-    setTimeout(setOpen(false), 1000)
+  const handleChangeConteudo = (event) => {
+    setConteudo(event.target.value);
   };
 
-  const [grids, setGrids] = useState([1]);
-
-  const addGrid = () => {
-    setGrids((prevGrids) => [...prevGrids, {}]);
+  const adicionarAnotacao = () => {
+    if (titulo && conteudo) {
+      const novaAnotacao = {
+        titulo: titulo,
+        conteudo: conteudo,
+      };
+      setAnotacoes([...anotacoes, novaAnotacao]);
+      setTitulo('');
+      setConteudo('');
+      setAlerta(true);
+    }
   };
 
-  const removeGrid = (index) => {
-    setGrids((prevGrids) => {
-      const newGrids = [...prevGrids];
-      newGrids.splice(index, 1);
-      return newGrids;
-    });
+  const excluirAnotacao = (index) => {
+    const novaLista = [...anotacoes];
+    novaLista.splice(index, 1);
+    setAnotacoes(novaLista);
+  };
+
+  const fecharAlerta = () => {
+    setAlerta(false);
   };
 
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-      {grids.map((grid, index) => (
-        <Grid container spacing={3} key={index} style={{ marginBottom: "20px" }}>
-          <Paper
-            style={{
-              padding: "10px",
-              margin: "10px",
-              width: "100%",
-              display: "flex",
-            }}
-          >
-            <TextareaAutosize
-              variant="outlined"
-              style={{ width: "700vh", resize: "none", minHeight: "60px" }}
-            />
-            <Container
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "flex-end",
-                gap: "10px",
-                maxHeight: "64px",
-                alignItems: "center",
-              }}
-            >
-              <Snackbar open={open}  autoHideDuration={2000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="success" variant="outlined" sx={{ width: '100%', backgroundColor: 'rgb(203,247,199)'}}>
-                  Anotações salvas com sucesso!
-                </Alert>
-              </Snackbar>
 
-              <Button variant="contained" color="primary" className="save" onClick={handleClick}>
-                <SaveIcon />
+{/* Cadastrar anotação */}
+      <Paper
+        style={{
+        paddingTop: "0px",
+        padding: "10px",
+        width: "100%"}}>
+
+  {/* Título do componente */}
+          <div>
+            <h1 style={{
+            textAlign: "center",
+            marginTop:"1px"
+            }}>Bloco de Notas</h1>
+          </div>
+
+  {/* Input: Título da anotação */}
+          <div>
+            <TextareaAutosize
+            value={titulo}
+            onChange={handleChangeTitulo}
+            placeholder="Digite o título da anotação..." 
+            style={{ width: "100vh", resize: "none", minHeight: "20px" }}/>
+          </div>
+
+  {/* Input: Conteúdo da anotação e botão salvar */}
+          <div style={{
+          marginTop: "0.2rem",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems:"center"}}>
+
+            <TextareaAutosize
+              value={conteudo}
+              onChange={handleChangeConteudo}
+              placeholder="Digite o conteúdo da anotação..."
+              variant="outlined"
+              style={{ width: "140vh", resize: "none", minHeight: "60px" }}/>
+
+            <Button variant="contained" color="primary" className="save" onClick={adicionarAnotacao}>
+              <SaveIcon />
+            </Button>
+          </div>
+        </Paper>
+
+{/* Alerta de anotação salva */}
+      <Snackbar open={alerta}  autoHideDuration={2000} onClose={fecharAlerta}>
+
+        <Alert onClose={fecharAlerta} severity="success" variant="outlined" sx={{ width: '100%', backgroundColor: 'rgb(203,247,199)'}}>Anotação salva com sucesso!
+        </Alert>
+      </Snackbar>
+
+{/* Adicionar anotações */}
+      <ul 
+      style={{
+      listStyle: "none",
+      padding: "0px"}}>
+
+        {anotacoes.map((anotacao, index) => (
+          <li key={index}>
+            <Paper
+              style={{
+              padding: "10px",
+              width: "100%",
+              marginBottom:"10px"}}>
+            <h2 style={{
+            textAlign:"center",
+            textDecoration:"underline",
+            padding: "5px",
+            wordBreak:"break-all",
+            margin:"5px"
+            }}>{anotacao.titulo}</h2>
+
+            <div style={{display: "flex",
+            flexDirection:"column"}}>
+              <p style={{borderStyle: "solid",
+              borderWidth:"1px",
+              padding: "5px",
+              wordBreak:"break-word",
+              Width: "140vh",
+              }}>{anotacao.conteudo}</p>
+
+              <Button
+                variant="contained"
+                color="primary" onClick={() => excluirAnotacao(index)}>
+                <CloseIcon />
               </Button>
-              {index === grids.length - 1 && grids.length > 1 ? ( 
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => removeGrid(index)}
-                  className="close"
-                >
-                  <CloseIcon />
-                </Button>
-              ) : (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  disabled
-                  style={{  backgroundColor: "#bbbbbb", cursor: "not-allowed" }}
-                >
-                  <CloseIcon />
-                </Button>
-              )}
-            </Container>
-          </Paper>
-        </Grid>
-      ))}
-      <Grid container justifyContent="center">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={addGrid}
-          style={{ height: "48px", width: "86px" }}
-        >
-          <AddIcon />
-        </Button>
-      </Grid>
+            </div>
+            </Paper>
+          </li>
+        ))}
+      </ul>
+
     </Container>
   );
 }
